5279e93ec5fc87a34e0a61a450e32a01319b06a8
Jujuad <juandradedias1@gmail.com>
2023-04-28 20:56:48 -0300

    aula 28/04 - JavaScript inicializacao

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//console.log("Olá Mundo!");
+
+var nr1 = "10";
+var nr1 = "5";
+
+console.log(parseInt(nr1)+parseInt(nr2));
+
+let nome1 = "Dubas";
+//let nome2 = "Serbatinho";
+//const nome3 = "Julia";
+
+if(nome1 != ""){
+    let nome1 = "Julia";
+}
+
+console.log(nome1);
