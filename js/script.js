e417de6bdd3675d19aefec8b0f1dca4b3958bcfd
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-05-03 20:52:33 -0300

    defer / addEventListener / getById / Math / Interpolacao

--- js/script.js
+++ js/script.js
@@ -2,17 +2,49 @@
 
 //console.log("Olá Mundo!");
 
-var nr1 = "10";
-var nr1 = "5";
+// var nr1 = "10";
+// var nr1 = "5";
 
-console.log(parseInt(nr1)+parseInt(nr2));
+// console.log(parseInt(nr1)+parseInt(nr2));
 
-let nome1 = "Dubas";
-//let nome2 = "Serbatinho";
-//const nome3 = "Julia";
+// let nome1 = "Dubas";
+// //let nome2 = "Serbatinho";
+// //const nome3 = "Julia";
+// nome = "Ale";
+// console.log(nome);
 
-if(nome1 != ""){
-    let nome1 = "Julia";
-}
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
 
-console.log(nome1);
+// var nome
+
+// console.log(nome1);
+
+/* <button value="click" id="meu-btn">Click</button> */
+
+// declaracao de button do html pelo id
+const btn = document.getElementById("meu-btn");
+
+// transformacao do btn em rgb
+// elemento.escutarEvento("evento", função)
+btn.addEventListener("click", function(){
+    // console.log(this.textContent)
+
+    // Função matemática Math
+    //randon = Retorna um numero aleatório entre 0 e 1. Ex: 0.23345678;
+    //floor  = Arrendonda o número para baixo.
+    //ceil  = Arrendonda o número para cima.
+    //round  = Arrendonda o número aleatório.
+
+    let r=0,g=0,b=0;
+
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    //this.setAttribute("NOME DO ATRIBUTO", "VALOR INICIAL OU ADICIONADO DO ATRIBUTO")
+    //this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+    //com interpolacao
+    this.setAttribute("style",`background-color:rgb(${r},${g},${b});`);
+});
